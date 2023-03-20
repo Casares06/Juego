624a36728dc3008702aac34a761687cc32fd6f05
Casares06 <127934908+Casares06@users.noreply.github.com>
2023-03-20 23:39:23 +0100

    Actualización 2.5
    Particle effect

--- My project (2)/Assets/Scripts/Bow.cs
+++ My project (2)/Assets/Scripts/Bow.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Bow : MonoBehaviour
 {
+    public ParticleSystem particleEffect;
+    public Transform playerPosition;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         PlayerController Pc = collision.GetComponent<PlayerController>();
 
         Pc.HasBow = true;
+        Instantiate(particleEffect, playerPosition);
         Destroy(gameObject);
     }
 }
