6c76f596490a135015011f4f79476d13e5cd9c18
Casares06 <127934908+Casares06@users.noreply.github.com>
2023-05-01 13:57:55 +0200

    Cold death upgraded

--- My project (2)/Assets/Scripts/World/ColdDeath.cs
+++ My project (2)/Assets/Scripts/World/ColdDeath.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ColdDeath : MonoBehaviour
 {
+    public UnityEvent<int, Vector2> takeDamage;
+
     public float freezetimer;
     public float hottimer;
+    private float takedamagetimer;
+    private float slider = 10;
+    public float numberTimer;
 
     public Color cold;
     public Color hot;
+
+    Damageable player;
+    UIManager UI;
 
     SpriteRenderer spriterenderer;
 
@@ -18,6 +27,8 @@ public class ColdDeath : MonoBehaviour
     void Start()
     {
         spriterenderer = GetComponent<SpriteRenderer>();
+        player = GameObject.Find("Player").GetComponent<Damageable>();
+        takedamagetimer = numberTimer;
     }
 
 
@@ -27,17 +38,42 @@ public class ColdDeath : MonoBehaviour
         if(Freezing && freezetimer >= 0 && !Freezed)
         {
             freezetimer -= Time.deltaTime;
-            spriterenderer.material.color = Color.Lerp(spriterenderer.material.color, cold, freezetimer/10000);
+            spriterenderer.material.color = Color.Lerp(spriterenderer.material.color, cold, slider/10000);
             hottimer = 10;
+
+            slider -= Time.deltaTime;
             
         }
         else if(Freezed && hottimer >= 0 && !Freezing)
         {
             Freezing = false;
             hottimer -= Time.deltaTime;
-            spriterenderer.material.color = Color.Lerp(spriterenderer.material.color, hot, hottimer/10000);
+            spriterenderer.material.color = Color.Lerp(spriterenderer.material.color, hot, slider/10000);
             freezetimer = 10;
+            slider += Time.deltaTime;
         }
+
+        if (slider <= 0)
+        {
+            takedamagetimer -= Time.deltaTime;
+
+            if(takedamagetimer <= 0)
+            {
+                player.Health -= 1;
+                CharacterEvents.characterDamaged.Invoke(GameObject.Find("Player"), 1);
+                takedamagetimer += numberTimer;
+            }
+        }
+
+        if(slider < 0)
+        {
+            slider = 0;
+        }
+        if(slider > 10)
+        {
+            slider = 10;
+        }
+
     }
 
     
