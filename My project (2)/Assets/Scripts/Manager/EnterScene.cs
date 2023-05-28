0ea354caf2ceaa13fec1314080332f3cc83ef902
Casares06 <127934908+Casares06@users.noreply.github.com>
2023-05-28 11:03:57 +0200

    Casa/Slide

--- My project (2)/Assets/Scripts/Manager/EnterScene.cs
+++ My project (2)/Assets/Scripts/Manager/EnterScene.cs
@@ -5,12 +5,59 @@ using UnityEngine;
 public class EnterScene : MonoBehaviour
 {
     public string lastExitName;
-    // Start is called before the first frame update
+    Damageable damageable;
+    GameObject player;
+    Animator animator;
+    Rigidbody2D rb;
+    ColdDeath cd;
+    public GameObject respawn;
+    public GameObject respawnUI;
+
+    Animator animatorUI;
+    public float respawnTimer = 3f;
+    
+
     void Start()
     {
         if(PlayerPrefs.GetString("LastExitName") == lastExitName)
         {
             PlayerManager.instance.transform.position = transform.position;
+        }
+
+        player = GameObject.Find("Player");
+
+        damageable = player.GetComponent<Damageable>();
+        animator = player.GetComponent<Animator>();
+        rb = player.GetComponent<Rigidbody2D>();
+        cd = player.GetComponent<ColdDeath>();
+        animatorUI = respawnUI.GetComponent<Animator>();
+        
+    }
+
+    void Update()
+    {
+        if(!damageable.IsAlive)
+        {
+            respawnTimer -= Time.deltaTime;
+            cd.slider = 10;
+            animatorUI.SetBool("Respawn", true);
+            
+            if(respawnTimer <= 0)
+            {
+                player.transform.position = respawn.transform.position;
+                damageable.Health = 100;
+                cd.intensity = 0;
+                rb.velocity = new Vector2(0,0);
+                damageable.IsAlive = true;
+                animatorUI.SetBool("Respawn", false);
+                
+            }
+            
+        }
+
+        if(damageable.IsAlive)
+        {
+            respawnTimer = 3f;
         }
     }
 
