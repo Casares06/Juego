f4cc447a63b3b20442abf2d56272b4bcf932c125
Casares06 <127934908+Casares06@users.noreply.github.com>
2023-05-30 22:32:26 +0200

    SomeChanges necromancer

--- My project (2)/Assets/Scripts/Enemies and NPC/NecromancerProyectile.cs
+++ My project (2)/Assets/Scripts/Enemies and NPC/NecromancerProyectile.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class NecromancerProyectile : MonoBehaviour
 {
     public GameObject necromancerProyectile;
+    public GameObject necromancerBolt;
+    public GameObject plasmaBall;
+
+    public Transform launcher;
     GameObject PC;
     Rigidbody2D rb;
     Animator animator;
@@ -20,6 +24,10 @@ public class NecromancerProyectile : MonoBehaviour
     private bool IsSliding;
 
     private float attackTimer = 3f;
+    public bool Angry;
+    public float hitTimer = 4f;
+    public bool Hit = true;
+    public int numHit = 0;
 
     void Awake()
     {
@@ -37,26 +45,61 @@ public class NecromancerProyectile : MonoBehaviour
 
     }
 
+    public void NecromancerBolts()
+    {
+        GameObject proyectile1 = Instantiate(necromancerBolt, new Vector2(Random.Range(-49,-56), transform.position.y - 2.6f), necromancerProyectile.transform.rotation);
+        GameObject proyectile2 = Instantiate(necromancerBolt, new Vector2(Random.Range(-42,-49), transform.position.y - 2.6f), necromancerProyectile.transform.rotation);
+        GameObject proyectile3 = Instantiate(necromancerBolt, new Vector2(Random.Range(-35,-42), transform.position.y - 2.6f), necromancerProyectile.transform.rotation);
+        GameObject proyectile4 = Instantiate(necromancerBolt, new Vector2(Random.Range(-29,-35), transform.position.y - 2.6f), necromancerProyectile.transform.rotation);
+        
+
+    }
+
+    public void NecromancerPlasmaBalls()
+    {
+        GameObject proyectile = Instantiate(plasmaBall, new Vector2(launcher.transform.position.x, launcher.transform.position.y -0.5f), plasmaBall.transform.rotation);
+        Vector3 origScale = proyectile.transform.localScale;
+        proyectile.transform.localScale = new Vector3(origScale.x * transform.localScale.x > 0 ? 1 : -1, origScale.y, origScale.z);
+
+        GameObject proyectile1 = Instantiate(plasmaBall, new Vector2(launcher.transform.position.x, launcher.transform.position.y + 0.5f), plasmaBall.transform.rotation);
+        Vector3 origScale1 = proyectile1.transform.localScale;
+        proyectile1.transform.localScale = new Vector3(origScale1.x * transform.localScale.x > 0 ? 1 : -1, origScale1.y, origScale1.z);
+
+    }
+
     public void OnHit(int damage, Vector2 knockback)
     {
         rb.velocity = new Vector2(0, rb.velocity.y + 0);
         animator.SetTrigger("Hit");
+        Hit = true;
 
     }
 
     void Update()
     {
+        if (Hit)
+        {
+            numHit += 1;
+            Hit = false;
+            hitTimer = 4f;
+
+            if(numHit >= 4)
+            {
+                Angry = true;
+            }
+        }
+
+        if (hitTimer <= 0)
+        {
+            numHit = 0;
+            Angry = false;
+            hitTimer = 4;
+        }
+
+        hitTimer -= Time.deltaTime;
         attackTimer -= Time.deltaTime;
 
-        if (PC.transform.position.x < transform.position.x && !IsSliding)
-        {
-            transform.localScale = new Vector2(-1.2f, transform.localScale.y);
-        }
-
-        if (PC.transform.position.x > transform.position.x && !IsSliding)
-        {
-            transform.localScale = new Vector2(1.2f, transform.localScale.y);
-        }
+        FlipDirection();
 
         Health = damageable.Health;
         MaxHealth = damageable.MaxHealth;
@@ -76,25 +119,59 @@ public class NecromancerProyectile : MonoBehaviour
     {
         animator.SetBool("IsAttacking", true);
         animator.SetBool("IsMoving", false);
+        animator.SetBool("Attack1", false);
+        animator.SetBool("Attack2", false);
+    }
+
+    void CastDarkBolt()
+    {
+        animator.SetBool("Attack1", true);
+        animator.SetBool("IsAttacking", false);
+        animator.SetBool("IsMoving", false);
+        animator.SetBool("Attack2", false);
+    }
+
+    void CastPlasmaBalls()
+    {
+        animator.SetBool("Attack1", false);
+        animator.SetBool("IsAttacking", false);
+        animator.SetBool("IsMoving", false);
+        animator.SetBool("Attack2", true);
     }
 
     void SlideAttack()
     {
+        FlipDirection();
         IsSliding = true;
         animator.SetBool("IsMoving", true);
         animator.SetBool("IsAttacking", false);
+        animator.SetBool("Attack1", false);
+        animator.SetBool("Attack2", false);
+        
         rb.velocity = new Vector2(slideSpeed * transform.localScale.x, rb.velocity.y);
     }
 
     void AttackAlternate()
     {
-        random = Random.Range(0,2);
+        random = Random.Range(0,4);
 
-        if (random == 0)
+        if (random == 0 && !Angry)
         {
             CastLightning();
         }
-        else SlideAttack();
+        else if (random == 1 && !Angry) SlideAttack();
+        else if (random == 2 && !Angry)CastDarkBolt();
+        else if (random == 3 && !Angry) CastPlasmaBalls();
+
+        if (numHit <= 4 && hitTimer >= 0 && Angry)
+        {
+            Debug.Log("Angry");
+            ResetAnimator();
+            SlideAttack();
+            numHit = 0;
+            attackTimer = 3;
+            Angry = false;
+        }
 
 
         attackTimer += 1;
@@ -106,7 +183,23 @@ public class NecromancerProyectile : MonoBehaviour
         attackTimer += 1;
         animator.SetBool("IsAttacking", false);
         animator.SetBool("IsMoving", false);
+        animator.SetBool("Attack1", false);
+        animator.SetBool("Attack2", false);
         rb.velocity = new Vector2(0,0);
         IsSliding = false;
+
+    }
+
+    void FlipDirection()
+    {
+        if (PC.transform.position.x < transform.position.x && !IsSliding)
+        {
+            transform.localScale = new Vector2(-1.2f, transform.localScale.y);
+        }
+
+        if (PC.transform.position.x > transform.position.x && !IsSliding)
+        {
+            transform.localScale = new Vector2(1.2f, transform.localScale.y);
+        }
     }
 }
--- My project (2)/Assets/Scripts/Player/ProyectileLauncher.cs
+++ My project (2)/Assets/Scripts/Player/ProyectileLauncher.cs
@@ -6,7 +6,7 @@ public class ProyectileLauncher : MonoBehaviour
 {
     public Transform launchPoint;
     public GameObject proyectilePrefab;
-    public GameObject necromancerProyectile;
+    public GameObject plasmaBall;
     PlayerController PC;
 
     void Awake()
@@ -27,11 +27,4 @@ public class ProyectileLauncher : MonoBehaviour
         }
     }
 
-    public void NecromancerLightning()
-    {
-        GameObject proyectile = Instantiate(necromancerProyectile, new Vector2(transform.position.x, transform.position.y + 5), necromancerProyectile.transform.rotation);
-        //Vector3 origScale = proyectile.transform.localScale;
-        //proyectile.transform.localScale = new Vector3(origScale.x * transform.localScale.x > 0 ? 1 : -1, origScale.y, origScale.z);
-
-    }
 }
